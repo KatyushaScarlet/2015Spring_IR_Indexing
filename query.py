1c9c72e0cdf685624d394a6a5cbb43e18d5b7b68
Raccoon <a60814billy@gmail.com>
2015-04-27 23:19:54 +0800

    fixed some bugs

--- query.py
+++ query.py
@@ -56,9 +56,11 @@ def query():
         docs_table = {}
         docs_set = set()
         docs_score = {}
+        # Calculate query's weight
         for term in query_string:
             if term in dicts:
                 term_index[term] = Index.read_index_by_offset(idx_file, dicts[term]).index[term]
+                # add doc# to set
                 for doc in term_index[term]:
                     docs_set.add(int(doc))
                 query_table[term] = {}
@@ -67,54 +69,65 @@ def query():
                 query_table[term]["idf"] = math.log(N / query_table[term]["df"], 10)
                 query_table[term]["w"] = (1 + math.log(query_table[term]["tf"])) * query_table[term]["idf"]
             else:
+                term_index[term] = {}
                 query_table[term] = {}
                 query_table[term]["tf"] = 1
                 query_table[term]["df"] = 0
                 query_table[term]["idf"] = 0
                 query_table[term]["w"] = 0
-                pass
+
+        # Calculate query's weight
         euclidean_length = 0
         while True:
             try:
                 element = str(docs_set.pop())
-                docs_table[element] = {}
-                for term in query_string:
-                    docs_table[element][term] = {}
-                    if element in term_index[term]:
-                        docs_table[element][term]["tf"] = len(term_index[term][str(element)])
-                    else:
-                        docs_table[element][term]["tf"] = 0
-                    euclidean_length += docs_table[element][term]["tf"] * docs_table[element][term]["tf"]
             except KeyError:
                 break
+            docs_table[element] = {}
+            for term in query_string:
+                docs_table[element][term] = {}
+                docs_table[element][term]["tf"] = 0
+                if element in term_index[term]:
+                    docs_table[element][term]["tf"] = len(term_index[term][str(element)])
+                euclidean_length += docs_table[element][term]["tf"] * docs_table[element][term]["tf"]
         euclidean_length = math.sqrt(euclidean_length)
+
+
         for doc in docs_table:
             for term in query_string:
-                docs_table[doc][term]["w"] = docs_table[doc][term]["tf"] / euclidean_length
+                if docs_table[doc][term]["tf"] > 0:
+                    docs_table[doc][term]["w"] = (1 + math.log(docs_table[doc][term]["tf"], 10)) * math.log(query_table[term]["df"], 10)
+                else:
+                    docs_table[doc][term]["w"] = 0
 
         query_len = 0
         for term in query_string:
             query_len += query_table[term]["w"] * query_table[term]["w"]
         query_len = math.sqrt(query_len)
+        print("query len", query_len)
 
         for doc in docs_table:
             up_part = 0
             doc_len = 0
-            for term in query_string:
-                up_part += docs_table[doc][term]["w"] * query_table[term]["w"]
-                doc_len += docs_table[doc][term]["w"] * docs_table[doc][term]["w"]
+            for terms in query_string:
+                up_part += docs_table[doc][terms]["w"] * query_table[terms]["w"]
+                doc_len += docs_table[doc][terms]["w"] * docs_table[doc][terms]["w"]
             docs_score[doc] = up_part / (math.sqrt(doc_len) * query_len)
+                # print(query_table['hello']['w'])
+                # print(docs_table['4727']['hello']['w'])
 
+                # print(docs_table)
+                # print(docs_score)
         print("doc#\tscore")
 
         for i in sorted(docs_score, key=docs_score.get, reverse=True):
             return_count -= 1
-            if return_count < 0: break
+            if return_count < 0:
+                break
             print("%d\t%.3f" % (int(i), docs_score[i]))
-
     else:
         exit()
-    pass
+        pass
 
 
 if __name__ == "__main__":
